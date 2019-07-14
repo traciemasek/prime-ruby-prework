b9185d14e86dca745a4c72fa798a775d9c9bc1f4
Tracie Masek <traciemasek@gmail.com>
2019-07-14 15:52:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false 
+  end
+  
+  for x in 2..(num / 2)
+    if num % x == 0 
+      return false
+    end
+  end
+  return true  
+end
