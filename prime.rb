093d54bd3d5499a5633e98333a4b24357e061bb3
Tracie Masek <traciemasek@gmail.com>
2019-07-14 15:38:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,10 @@ def prime?(num)
     return false 
   else
     for x in 2..num
-      if num % x == 0 
-        return false
-      else 
+      if num % x != 0 
         return true
+      else 
+        return false
       end
     end
   end
